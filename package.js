5f9725b1b4abe6e264ced2e9fb68e4e1bebb3d41
Victor Parpoil <vparpoil@gmail.com>
2024-06-10 14:39:04 +0200

    * update package.js and bump to 1.1.0

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
-  name:    'zuuk:stale-session',
-  summary: 'Stale session and session timeout handling for meteorjs',
-  git:     "https://github.com/lindleycb/meteor-stale-session.git",
-  version: "1.0.9"
+    name: "zuuk:stale-session",
+    summary: "Stale session and session timeout handling for meteorjs",
+    git: "https://github.com/lindleycb/meteor-stale-session.git",
+    version: "1.1.0",
 });
 
-Package.onUse(function(api) {
-    api.use('accounts-base@1.0.0', ['client','server']);
-    api.use('jquery@1.0.0', 'client');
-    api.addFiles('client.js', 'client');
-    api.addFiles('server.js', 'server');
+Package.onUse(function (api) {
+    api.versionsFrom("3.0-rc.1");
+
+    api.use("accounts-base@3.0.0-rc300.2", ["client", "server"]);
+    api.use("jquery", "client");
+    api.addFiles("client.js", "client");
+    api.addFiles("server.js", "server");
 });
